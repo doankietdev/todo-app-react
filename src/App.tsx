d6be0c05b2a8e72318cc9d81406a34a6cc55be34
doankietdev <doankietdev@gmail.com>
2025-05-20 13:21:34 +0700

    chore: add codebase

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen space-y-20">
+      <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Todo App
+      </h1>
+    </main>
+  );
+}
+
+export default App;
