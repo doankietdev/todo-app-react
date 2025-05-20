33ac8afb147c319b2ff02de07dc11a95ddee7647
doankietdev <doankietdev@gmail.com>
2025-05-20 16:40:56 +0700

    fix: responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,14 @@ import TodoList from "./components/TodoList/TodoList";
 
 function App() {
   return (
-    <main className="w-[500px] mx-auto flex flex-col bg-white p-5 shadow-[0_0_10px_4px_#dbdbdb] rounded-md h-[90vh]">
+    <main className="lg:w-[800px] mx-auto flex flex-col bg-white p-5 shadow-[0_0_10px_4px_#dbdbdb] rounded-md h-[90vh]">
       
       <h1 className="text-4xl font-semibold text-center">Todo App</h1>
 
       <div className="py-6 h-full overflow-y-hidden">
-
         <AddTodo />
         <TodoList />
       </div>
-
-      <div></div>
     </main>
   );
 }
